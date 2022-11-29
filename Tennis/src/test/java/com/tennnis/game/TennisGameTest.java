2fccbdfeee6f3a21ca42d4f4cc33405ce0459f51
Ayushi <HP@DESKTOP-KQA8LBN>
2022-11-29 17:35:54 +0530

    Reafctor : Using Parameterized Test Case to use same test over and over again using different values to increase readability and to reuse same code.

--- Tennis/src/test/java/com/tennnis/game/TennisGameTest.java
+++ Tennis/src/test/java/com/tennnis/game/TennisGameTest.java
@@ -2,36 +2,33 @@ package com.tennnis.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TennisGameTest {
 
 	private TennisGame tennisGame;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		tennisGame = new TennisGame("PlayerOneName", "PlayerTwoName");
 	}
 
-	@Test
-	public void LoveAllTest() {
-		assertThat(tennisGame.getGameScore()).isEqualTo("Love All");
+	@ParameterizedTest
+	@CsvSource({
+		"0,0,Love All" ,
+		"1,1,Fifteen All",
+		"2,2,Thirty All" 
+	})
+	void parameterizedTestCaseforEachTest(int playerOneScore, int playerTwoScore, String expectedScore) {
+		for(int i=0; i<playerOneScore; i++) {
+			tennisGame.playerOneScored();
+		}
+		for(int i=0; i<playerTwoScore; i++) {
+			tennisGame.playerTwoScored();
+		}
+		assertThat(tennisGame.getGameScore()).isEqualTo(expectedScore);
 	}
 
-	@Test
-	public void FifteenAllTest() {
-		tennisGame.playerOneScored();
-		tennisGame.playerTwoScored();
-		assertThat(tennisGame.getGameScore()).isEqualTo("Fifteen All");
-	}
-
-	@Test
-	public void ThirtyAllTest() {
-		tennisGame.playerOneScored();
-		tennisGame.playerTwoScored();
-		tennisGame.playerOneScored();
-		tennisGame.playerTwoScored();
-		assertThat(tennisGame.getGameScore()).isEqualTo("Thirty All");
-	}
 }
